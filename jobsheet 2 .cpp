873071156f463f4d6ce65827395bea245d876715
haidir2104 <64574920+haidir2104@users.noreply.github.com>
2020-05-18 01:36:49 +0800

    Add files via upload

---
+++ jobsheet 2 .cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+int main (){
+	string x [9];
+	cout<<"masukkan nim : "<<endl;
+	cin>>x[0];
+	cout<<"masukkan nim : "<<endl;
+	cin>>x[1];
+            cout<<"masukkan nim : "<<endl;
+	cin>>x[2];
+	cout<<"masukkan nim : "<<endl;
+	cin>>x[3];
+            cout<<"masukkan nim : "<<endl;
+	cin>>x[4];
+cout<<"masukkan nim : "<<endl;
+cin>>x[5];
+cout<<"masukkan nim : "<<endl;
+cin>>x[6];
+cin>>x[7];
+cout<<"masukkan nim : "<<endl;
+cin>>x[8];
+
+
+return 0;}
+
